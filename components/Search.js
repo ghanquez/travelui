667ed2e77a531f221b96c282a6cc03217a16efd3
ghanquez <7419028+ghanquez@users.noreply.github.com>
2021-12-11 20:33:04 +0100

    [SONAR] Fix sonar warnings

--- components/Search.js
+++ components/Search.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import SearchStyles, {
   SearchItemTownsStyle,
   SearchItemHalfFieldStyle,
@@ -12,7 +11,6 @@ import {
   faHome,
   faCarSide,
   faMapMarkerAlt,
-  faMarker,
   faCalendarAlt,
   faUsers,
   faPlus
